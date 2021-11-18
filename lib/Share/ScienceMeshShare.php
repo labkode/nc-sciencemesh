1fef2df32fb29d36253da46e2bb535d7f0fe3fa6
bänz <benz.schenk@brokkoli.be>
2021-11-18 13:15:21 +0100

    Continue work on ScienceMeshShare: Added getter and setters for ScienceMesh fields except times as well as IShare node and id

--- lib/Share/ScienceMeshShare.php
+++ lib/Share/ScienceMeshShare.php
@@ -2,8 +2,11 @@
 
 namespace OCA\ScienceMesh\Share;
 
+use OCP\Files\Node;
+use OCP\Files\NotFoundException;
+use OCP\Share\Exceptions\IllegalIDChangeException;
+
 class ScienceMeshShare {
-	private $scienceMeshId;
 	private $scienceMeshResourceId;
 	private $scienceMeshPermissions;
 	private $scienceMeshGrantee;
@@ -11,4 +14,90 @@ class ScienceMeshShare {
 	private $scienceMeshCreator;
 	private $scienceMeshCTime;
 	private $scienceMeshMTime;
+
+	private $id;
+	private $node;
+
+	public function setScienceMeshId($id) {
+		$this->setId($id);
+	}
+
+	public function getScienceMeshId() {
+		return $this->getId();
+	}
+
+	public function setScienceMeshResourceId($rid) {
+		$this->scienceMeshResourceId = $rid;
+	}
+
+	public function getScienceMeshResourceId() {
+		return $this->scienceMeshResourceId;
+	}
+
+	public function setScienceMeshPermissions($permissions) {
+		$this->scienceMeshPermissions = $permissions;
+	}
+
+	public function getScienceMeshPermissions() {
+		return $this->scienceMeshPermissions;
+	}
+
+	public function setScienceMeshGrantee($grantee) {
+		$this->scienceMeshGrantee = $grantee;
+	}
+
+	public function getScienceMeshGrantee() {
+		return $this->scienceMeshGrantee;
+	}
+
+	public function setScienceMeshOwner($owner) {
+		$this->scienceMeshOwner = $owner;
+	}
+
+	public function getScienceMeshOwner() {
+		return $this->scienceMeshOwner;
+	}
+
+	public function setScienceMeshCreator($creator) {
+		$this->scienceMeshCreator = $creator;
+	}
+
+	public function getScienceMeshCreator() {
+		return $this->scienceMeshCreator;
+	}
+
+	public function setId($id) {
+		if (!is_string($id)) {
+			throw(new \InvalidArgumentException(
+				__CLASS__ .
+				": Id has to be of type String."
+			));
+		} elseif ($this->id != null) {
+			throw(new IllegalIDChangeException(
+				__CLASS__ .
+				": It is only allowed to set the internal id of a share once."
+			));
+		} else {
+			$this->id = $id;
+			return $this;
+		}
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setNode(Node $node) {
+		$this->node = $node;
+		return $this;
+	}
+
+	public function getNode() {
+		try {
+			$nodeId = $this->node->getId();
+		} catch (NotFoundException $e) {
+			throw $e;
+		}
+		return $this->node;
+	}
 }
